50d30e092038239f665c9bf0f64a0ee41db16488
Yasaswini2105 <58051144+Yasaswini2105@users.noreply.github.com>
2020-12-08 23:25:43 -0500

    First_commit
    Object detection .py files.

---
+++ pascal_ds.py
@@ -0,0 +1,246 @@
+
+# evaluate the mask rcnn model on the kangaroo dataset
+from os import listdir
+from xml.etree import ElementTree
+from numpy import zeros
+from numpy import asarray
+from numpy import expand_dims
+from numpy import mean
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+from mrcnn.utils import Dataset
+from mrcnn.utils import compute_ap
+from mrcnn.model import load_image_gt
+from mrcnn.model import mold_image
+
+# function to extract bounding boxes from an annotation file
+
+class pascal_voc_dataset(Dataset):
+    def load_dataset(self, dataset_dir, is_train=True):
+        # define one class
+        self.add_class("dataset", 1, "voc")
+        # define data locations
+        images_dir = dataset_dir + '/JPEGImages/'
+        annotations_dir = dataset_dir + '/Annotations/'
+        # find all images
+        for filename in listdir(images_dir):
+            # extract image id
+            image_id = filename[:-4]
+            # skip bad images
+            if image_id in ['000091']:
+                continue
+                # skip all images after 150 if we are building the train set
+            if is_train and int(image_id) >= 150:
+                continue
+                # skip all images before 150 if we are building the test/val set
+            if not is_train and int(image_id) < 150:
+                continue
+            img_path = images_dir + filename
+            ann_path = annotations_dir + image_id + '.xml'
+            # add to dataset
+            self.add_image('dataset', image_id=image_id,
+                           path=img_path, annotation=ann_path)
+
+    def extract_boxes(self, filename):
+        # load and parse the file
+        tree = ElementTree.parse(filename)
+        root = tree.getroot()
+        # extract each bounding box
+        boxes = list()
+        names = list()
+        for box in root.findall('.//bndbox'):
+            xmin = int(box.find('xmin').text)
+            ymin = int(box.find('ymin').text)
+            xmax = int(box.find('xmax').text)
+            ymax = int(box.find('ymax').text)
+
+            coors = [xmin, ymin, xmax, ymax]
+            boxes.append(coors)
+        for name in root.findall('.//object'):
+            n = name.find('name').text
+
+            names.append(n)
+            # extract image dimensions
+        width = int(root.find('.//size/width').text)
+        height = int(root.find('.//size/height').text)
+        return boxes, width, height, names
+
+    def load_mask(self, image_id):
+        # get details of image
+        info = self.image_info[image_id]
+        # define box file location
+        path = info['annotation']
+        # load XML
+        boxes, w, h, n = self.extract_boxes(path)
+        print(n)
+        # create one array for all masks, each on a different channel
+        masks = zeros([h, w, len(boxes)], dtype='uint8')
+        # create masks
+        class_ids = list()
+        for i in range(len(boxes)):
+            box = boxes[i]
+            row_s, row_e = box[1], box[3]
+            col_s, col_e = box[0], box[2]
+            masks[row_s:row_e, col_s:col_e, i] = 1
+            class_ids.append(self.class_names.index('voc'))
+
+        return masks, asarray(class_ids, dtype='int32')
+
+    def image_reference(self, image_id):
+        info = self.image_info[image_id]
+        return info['path']
+
+
+
+def plot_actual_vs_predicted(dataset, model, cfg, n_images=5):
+    # load image and mask
+    for i in range(n_images):
+        pyplot.figure(figsize=(20,20))
+        # load the image and mask
+        image = dataset.load_image(i)
+        mask, _ = dataset.load_mask(i)
+        # convert pixel values (e.g. center)
+        scaled_image = mold_image(image, cfg)
+        # convert image into one sample
+        sample = expand_dims(scaled_image, 0)
+        # make prediction
+        yhat = model.detect(sample, verbose=0)[0]
+        # define subplot
+        pyplot.subplot(n_images, 2, i*2+1)
+        # plot raw pixel data
+        pyplot.imshow(image)
+        pyplot.title('Actual')
+        # plot masks
+        for j in range(mask.shape[2]):
+            pyplot.imshow(mask[:, :, j], cmap='gray', alpha=0.3)
+        # get the context for drawing boxes
+        pyplot.subplot(n_images, 2, i*2+2)
+        # plot raw pixel data
+        pyplot.imshow(image)
+        pyplot.title('Predicted')
+        ax = pyplot.gca()
+        # plot each box
+        for box in yhat['rois']:
+            # get coordinates
+            y1, x1, y2, x2 = box
+            # calculate width and height of the box
+            width, height = x2 - x1, y2 - y1
+            # create the shape
+            rect = Rectangle((x1, y1), width, height, fill=False, color='red')
+            # draw the box
+            ax.add_patch(rect)
+    # show the figure
+
+    pyplot.show()
+
+
+
+
+def evaluate_model(dataset, model, cfg):
+	APs = list()
+	for image_id in dataset.image_ids:
+		# load image, bounding boxes and masks for the image id
+		image, image_meta, gt_class_id, gt_bbox, gt_mask = load_image_gt(dataset, cfg, image_id, use_mini_mask=False)
+		# convert pixel values (e.g. center)
+		scaled_image = mold_image(image, cfg)
+		# convert image into one sample
+		sample = expand_dims(scaled_image, 0)
+		# make prediction
+		yhat = model.detect(sample, verbose=0)
+		# extract results for first sample
+		r = yhat[0]
+		# calculate statistics, including AP
+		AP, _, _, _ = compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"], r["class_ids"], r["scores"], r['masks'])
+		# store
+		APs.append(AP)
+	# calculate the mean AP across all images
+	mAP = mean(APs)
+	return mAP
+train_set = pascal_voc_dataset()
+train_set.load_dataset('voc', is_train=True)
+train_set.prepare()
+print('Train: %d' % len(train_set.image_ids))
+# load an image
+test_set = pascal_voc_dataset()
+test_set.load_dataset('voc', is_train=False)
+test_set.prepare()
+print('Test: %d' % len(test_set.image_ids))
+# prepare config
+class Pascal_voc_Config(Config):
+    # Give the configuration a recognizable name
+    NAME = "Pascal_voc_Config"
+    # Number of classes (background + kangaroo)
+    NUM_CLASSES = 1 + 1
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 131
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+# plot a number of photos with ground truth and predictions
+config = Pascal_voc_Config()
+# plot first few images
+config = Pascal_voc_Config()
+config.display()
+model = MaskRCNN(mode='training', model_dir='./', config=config)
+model.load_weights('mask_rcnn_coco.h5', by_name=True, exclude=[
+                   "mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
+    
+    
+model_path = 'pascal_voc_config20201204T0050/mask_rcnn_pascal_voc_config_0004.h5'
+model.load_weights(model_path, by_name=True)
+# plot predictions for train dataset
+plot_actual_vs_predicted(train_set, model, config)
+# plot predictions for test dataset
+plot_actual_vs_predicted(test_set, model, config)    
+
+
+'''
+    # plot first few images
+for i in range(9):
+	# define subplot
+	pyplot.subplot(330 + 1 + i)
+	# plot raw pixel data
+	image = train_set.load_image(i)
+	pyplot.imshow(image)
+	# plot all masks
+	mask, _ = train_set.load_mask(i)
+	for j in range(mask.shape[2]):
+		pyplot.imshow(mask[:, :, j], cmap='gray', alpha=0.3)
+# show the figure
+pyplot.show()
+# show the figure
+pyplot.show()
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+#model.train(train_set, test_set, learning_rate=config.LEARNING_RATE, epochs=5, layers='heads')
+cfg = Pascal_voc_Config()
+# define the model
+model = MaskRCNN(mode='inference', model_dir='./', config=cfg)
+# load model weights
+model_path = 'mask_rcnn_pascal_voc_config_0005.h5'
+
+model.load_weights(model_path, by_name=True)
+# plot predictions for train dataset
+plot_actual_vs_predicted(train_set, model, cfg)
+# plot predictions for test dataset
+plot_actual_vs_predicted(test_set, model, cfg)
+# evaluate model on training dataset
+train_mAP = evaluate_model(train_set, model, cfg)
+print("Train mAP: %.3f" % train_mAP)
+# evaluate model on test dataset
+test_mAP = evaluate_model(test_set, model, cfg)
+print("Test mAP: %.3f" % test_mAP)
+'''
---
+++ pascal_model.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 19:09:37 2020
+
+@author: vidhursavyasachin
+"""
+
+
+import os
+import random
+import math
+from datetime import datetime
+from collections import Counter
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+mpl.use('TkAgg')
+import cv2
+from PIL import Image
+from pathlib import Path
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from sklearn.model_selection import train_test_split
+import xml.etree.ElementTree as ET
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+images_path = Path('voc/JPEGImages/')
+annotations_path = Path('voc/Annotations')
+def filelist(root, file_type):
+    """Returns a fully-qualified list of filenames under root directory"""
+    return [os.path.join(directory_path, f) for directory_path, directory_name, 
+            files in os.walk(root) for f in files if f.endswith(file_type)]
+def generate_train_df (anno_path):
+    annotations = filelist(anno_path, '.xml')
+    anno_list = []
+    for anno_path in annotations:
+        root = ET.parse(anno_path).getroot()
+        anno = {}
+        anno['filename'] = Path(str(images_path) + '/'+ root.find("./filename").text)
+        anno['width'] = root.find("./size/width").text
+        anno['height'] = root.find("./size/height").text
+        anno['class'] = root.find("./object/name").text
+        anno['xmin'] = int(root.find("./object/bndbox/xmin").text)
+        anno['ymin'] = int(root.find("./object/bndbox/ymin").text)
+        anno['xmax'] = int(root.find("./object/bndbox/xmax").text)
+        anno['ymax'] = int(root.find("./object/bndbox/ymax").text)
+        anno_list.append(anno)
+    return pd.DataFrame(anno_list)
+df_train = generate_train_df(annotations_path)
+class_dict = {}
+for i in range(0,len(df_train['class'].unique())):
+    class_dict[df_train['class'].unique()[i]] = i
+df_train['class'] = df_train['class'].apply(lambda x:  class_dict[x])
+
+print(df_train.shape)
+df_train.head()
+def read_image(path):
+    return cv2.cvtColor(cv2.imread(str(path)), cv2.COLOR_BGR2RGB)
+def create_mask(bb, x):
+    """Creates a mask for the bounding box of same shape as image"""
+    rows,cols,*_ = x.shape
+    Y = np.zeros((rows, cols))
+    bb = bb.astype(np.int)
+    Y[bb[0]:bb[2], bb[1]:bb[3]] = 1.
+    return Y
+
+def mask_to_bb(Y):
+    """Convert mask Y to a bounding box, assumes 0 as background nonzero object"""
+    cols, rows = np.nonzero(Y)
+    if len(cols)==0: 
+        return np.zeros(4, dtype=np.float32)
+    top_row = np.min(rows)
+    left_col = np.min(cols)
+    bottom_row = np.max(rows)
+    right_col = np.max(cols)
+    return np.array([left_col, top_row, right_col, bottom_row], dtype=np.float32)
+
+def create_bb_array(x):
+    """Generates bounding box array from a train_df row"""
+    return np.array([x[5],x[4],x[7],x[6]])
+def resize_image_bb(read_path,write_path,bb,sz):
+    """Resize an image and its bounding box and write image to new path"""
+    im = read_image(read_path)
+    im_resized = cv2.resize(im, (int(1.49*sz), sz))
+    Y_resized = cv2.resize(create_mask(bb, im), (int(1.49*sz), sz))
+    new_path = str(write_path/read_path.parts[-1])
+    cv2.imwrite(new_path, cv2.cvtColor(im_resized, cv2.COLOR_RGB2BGR))
+    return new_path, mask_to_bb(Y_resized)
+new_paths = []
+new_bbs = []
+train_path_resized = Path('./voc/JPEGImages')
+for index, row in df_train.iterrows():
+    new_path,new_bb = resize_image_bb(row['filename'], train_path_resized, create_bb_array(row.values),300)
+    new_paths.append(new_path)
+    new_bbs.append(new_bb)
+df_train['new_path'] = new_paths
+df_train['new_bb'] = new_bbs
+
+im = cv2.imread(str(df_train.values[58][0]))
+bb = create_bb_array(df_train.values[58])
+print(im.shape)
+Y = create_mask(bb, im)
+mask_to_bb(Y)
+
+plt.imshow(im)
+plt.imshow(Y, cmap='gray')
+def crop(im, r, c, target_r, target_c): 
+    return im[r:r+target_r, c:c+target_c]
+
+# random crop to the original size
+def random_crop(x, r_pix=8):
+    """ Returns a random crop"""
+    r, c,*_ = x.shape
+    c_pix = round(r_pix*c/r)
+    rand_r = random.uniform(0, 1)
+    rand_c = random.uniform(0, 1)
+    start_r = np.floor(2*rand_r*r_pix).astype(int)
+    start_c = np.floor(2*rand_c*c_pix).astype(int)
+    return crop(x, start_r, start_c, r-2*r_pix, c-2*c_pix)
+
+def center_crop(x, r_pix=8):
+    r, c,*_ = x.shape
+    c_pix = round(r_pix*c/r)
+    return crop(x, r_pix, c_pix, r-2*r_pix, c-2*c_pix)
+
+
+def rotate_cv(im, deg, y=False, mode=cv2.BORDER_REFLECT, interpolation=cv2.INTER_AREA):
+    """ Rotates an image by deg degrees"""
+    r,c,*_ = im.shape
+    M = cv2.getRotationMatrix2D((c/2,r/2),deg,1)
+    if y:
+        return cv2.warpAffine(im, M,(c,r), borderMode=cv2.BORDER_CONSTANT)
+    return cv2.warpAffine(im,M,(c,r), borderMode=mode, flags=cv2.WARP_FILL_OUTLIERS+interpolation)
+
+def random_cropXY(x, Y, r_pix=8):
+    """ Returns a random crop"""
+    r, c,*_ = x.shape
+    c_pix = round(r_pix*c/r)
+    rand_r = random.uniform(0, 1)
+    rand_c = random.uniform(0, 1)
+    start_r = np.floor(2*rand_r*r_pix).astype(int)
+    start_c = np.floor(2*rand_c*c_pix).astype(int)
+    xx = crop(x, start_r, start_c, r-2*r_pix, c-2*c_pix)
+    YY = crop(Y, start_r, start_c, r-2*r_pix, c-2*c_pix)
+    return xx, YY
+
+def transformsXY(path, bb, transforms):
+    x = cv2.imread(str(path)).astype(np.float32)
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)/255
+    Y = create_mask(bb, x)
+    if transforms:
+        rdeg = (np.random.random()-.50)*20
+        x = rotate_cv(x, rdeg)
+        Y = rotate_cv(Y, rdeg, y=True)
+        if np.random.random() > 0.5: 
+            x = np.fliplr(x).copy()
+            Y = np.fliplr(Y).copy()
+        x, Y = random_cropXY(x, Y)
+    else:
+        x, Y = center_crop(x), center_crop(Y)
+    return x, mask_to_bb(Y)
+
+def create_corner_rect(bb, color='red'):
+    bb = np.array(bb, dtype=np.float32)
+    return plt.Rectangle((bb[1], bb[0]), bb[3]-bb[1], bb[2]-bb[0], color=color,
+                         fill=False, lw=3)
+
+def show_corner_bb(im, bb):
+    plt.imshow(im)
+    plt.gca().add_patch(create_corner_rect(bb))
+    
+    
+    
+#original
+im = cv2.imread(str(df_train.values[68][8]))
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+show_corner_bb(im, df_train.values[68][9])
+
+# after transformation
+im, bb = transformsXY(str(df_train.values[68][8]),df_train.values[68][9],True )
+show_corner_bb(im, bb)
+
+
+df_train = df_train.reset_index()
+X = df_train[['new_path', 'new_bb']]
+Y = df_train['class']
+
+X_train, X_val, y_train, y_val = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+
+def normalize(im):
+    """Normalizes images with Imagenet stats."""
+    imagenet_stats = np.array([[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]])
+    return (im - imagenet_stats[0])/imagenet_stats[1]
+class PascalVOC(Dataset):
+    def __init__(self, paths, bb, y, transforms=False):
+        self.transforms = transforms
+        self.paths = paths.values
+        self.bb = bb.values
+        self.y = y.values
+    def __len__(self):
+        return len(self.paths)
+    
+    def __getitem__(self, idx):
+        path = self.paths[idx]
+        y_class = self.y[idx]
+        x, y_bb = transformsXY(path, self.bb[idx], self.transforms)
+        x = normalize(x)
+        x = np.rollaxis(x, 2)
+        return x, y_class, y_bb
+train_ds = PascalVOC(X_train['new_path'],X_train['new_bb'] ,y_train, transforms=True)
+valid_ds = PascalVOC(X_val['new_path'],X_val['new_bb'],y_val)
+batch_size = 64
+train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+valid_dl = DataLoader(valid_ds, batch_size=batch_size)
+class BB_model(nn.Module):
+    def __init__(self):
+        super(BB_model, self).__init__()
+        resnet = models.resnet34(pretrained=True)
+        layers = list(resnet.children())[:8]
+        self.features1 = nn.Sequential(*layers[:6])
+        self.features2 = nn.Sequential(*layers[6:])
+        self.classifier = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 4))
+        self.bb = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 4))
+        
+    def forward(self, x):
+        x = self.features1(x)
+        x = self.features2(x)
+        x = F.relu(x)
+        x = nn.AdaptiveAvgPool2d((1,1))(x)
+        x = x.view(x.shape[0], -1)
+        return self.classifier(x), self.bb(x)
+def update_optimizer(optimizer, lr):
+    for i, param_group in enumerate(optimizer.param_groups):
+        param_group["lr"] = lr
+def train_epocs(model, optimizer, train_dl, val_dl, epochs=10,C=1000):
+    idx = 0
+    for i in range(epochs):
+        model.train()
+        total = 0
+        sum_loss = 0
+        for x, y_class, y_bb in train_dl:
+            batch = y_class.shape[0]
+            x = x.cuda().float()
+            y_class = y_class.cuda()
+            y_bb = y_bb.float()
+            out_class, out_bb = model(x)
+            loss_class = F.cross_entropy(out_class, y_class, reduction="sum")
+            loss_bb = F.l1_loss(out_bb, y_bb, reduction="none").sum(1)
+            loss_bb = loss_bb.sum()
+            loss = loss_class + loss_bb/C
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            idx += 1
+            total += batch
+            sum_loss += loss.item()
+        train_loss = sum_loss/total
+        val_loss, val_acc = val_metrics(model, valid_dl, C)
+        print("train_loss %.3f val_loss %.3f val_acc %.3f" % (train_loss, val_loss, val_acc))
+    return sum_loss/total
+def val_metrics(model, valid_dl, C=1000):
+    model.eval()
+    total = 0
+    sum_loss = 0
+    correct = 0 
+    for x, y_class, y_bb in valid_dl:
+        batch = y_class.shape[0]
+        x = x.cuda().float()
+        y_class = y_class.cuda()
+        y_bb = y_bb.cuda().float()
+        out_class, out_bb = model(x)
+        loss_class = F.cross_entropy(out_class, y_class, reduction="sum")
+        loss_bb = F.l1_loss(out_bb, y_bb, reduction="none").sum(1)
+        loss_bb = loss_bb.sum()
+        loss = loss_class + loss_bb/C
+        _, pred = torch.max(out_class, 1)
+        correct += pred.eq(y_class).sum().item()
+        sum_loss += loss.item()
+        total += batch
+    return sum_loss/total, correct/total
+model = BB_model()
+parameters = filter(lambda p: p.requires_grad, model.parameters())
+optimizer = torch.optim.Adam(parameters, lr=0.006)
+
+train_epocs(model, optimizer, train_dl, valid_dl, epochs=15)
+# if the system doesn't have cuda, run the code given below to get the reuslts. 
+import torchvision
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+df_train['class'].unique()
+model.eval()
+COCO_INSTANCE_CATEGORY_NAMES =['person', 'boat', 'cat', 'chair', 'bird', 'bicycle', 'sofa',
+       'tvmonitor', 'car', 'dog', 'diningtable', 'motorbike', 'bottle',
+       'aeroplane', 'cow', 'horse', 'bus', 'pottedplant', 'train',
+       'sheep']
+from torchvision import transforms as T
+def get_prediction(img_path, threshold):
+  img = Image.open(img_path) # Load the image
+  transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
+  img = transform(img) # Apply the transform to the image
+  pred = model([img]) # Pass the image to the model
+  pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
+  pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
+  pred_score = list(pred[0]['scores'].detach().numpy())
+  pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
+  pred_boxes = pred_boxes[:pred_t+1]
+  pred_class = pred_class[:pred_t+1]
+  return pred_boxes, pred_class
+def object_detection_api(img_path, threshold=0.5, rect_th=3, text_size=3, text_th=3):
+
+  boxes, pred_cls = get_prediction(img_path, threshold) # Get predictions
+  img = cv2.imread(img_path) # Read image with cv2
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
+  for i in range(len(boxes)):
+    cv2.rectangle(img, boxes[i][0], boxes[i][1],color=(0, 255, 0), thickness=rect_th) # Draw Rectangle with the coordinates
+    cv2.putText(img,pred_cls[i], boxes[i][0],  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th) # Write the prediction class
+  plt.figure(figsize=(20,30)) # display the output image
+  plt.imshow(img)
+  plt.xticks([])
+  plt.yticks([])
+  plt.show()
+  
+object_detection_api('./voc/JPEGImages/000002.jpg', threshold=0.8)
+
+
